7276080b557303e0e4ba6b8751fc39d3bdb9835d
bamdart <bamdart@gmail.com>
2018-03-27 21:34:34 +0800

    build successful

---
+++ Assets/OpenWebsiteOnHeadsetRemove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenWebsiteOnHeadsetRemove : MonoBehaviour
+{
+    #region Serialized
+    public OVRManager m_OVRManager;
+    #endregion
+
+    #region Variables
+    private bool m_UserPresent = true;
+    private bool m_HasOpenedURL = false;
+    #endregion
+
+    #region Lifecycle
+    private void Update()
+    {
+#if UNITY_ANDROID
+        bool isUserPresent = m_OVRManager.isUserPresent;
+
+        if (m_UserPresent != isUserPresent)
+        {
+            if (isUserPresent == false && m_HasOpenedURL == false && Application.isEditor == false)
+            {
+                m_HasOpenedURL = true;
+                Application.OpenURL("http://www.glitchers.com");
+            }
+        }
+
+        m_UserPresent = isUserPresent;
+#endif
+    }
+    #endregion
+}
---
+++ Assets/VR_camera_move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR_camera_move : MonoBehaviour {
+    public Transform vrCamera;
+    public float moveSpeed = 5.0f;
+    private CharacterController CharCont;
+
+    // Use this for initialization
+    void Start()
+    {
+        CharCont = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+            CharCont.SimpleMove(forward * moveSpeed * Time.deltaTime);
+        }
+    }
+}
